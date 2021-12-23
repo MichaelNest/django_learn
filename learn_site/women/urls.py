165b9cc16c42e43f4a395e5500d09572e194f41a
Python <python@example.com>
2021-12-23 18:00:05 +0200

    Lesson_6 - Templates

--- learn_site/women/urls.py
+++ learn_site/women/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, re_path
-from .views import index, cat, archive
+from .views import index, cat, archive, about
 
 urlpatterns = [
     path('', index, name='home'),
     path('categories/<int:cat_id>/', cat),
+    path('about/', about, name='about_page'),
     # path('categories/<slug:cat>/', cat)
-    re_path(r'^archive/(?P<year>[0-9]{4})/', archive)
+    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
 ]
--- learn_site/women/views.py
+++ learn_site/women/views.py
@@ -1,8 +1,18 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect
+from women.models import Women
 
+
+menu = ['About Site', 'Add Article', 'Exit']
 def index(request):
-    return HttpResponse('Page of Women App')
+    posts = Women.objects.all()
+    context = {'posts': posts, 'menu': menu, 'title': 'Master Page'}
+    # return HttpResponse('Page of Women App')
+    return render(request, 'women/index.html', context)
+
+def about(request):
+    context = {'menu': menu, 'title': 'About Site'}
+    return render(request, 'women/about.html', context)
 
 def cat(request, cat_id):
     if request.GET:
